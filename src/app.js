303323758466a2084c0f255c11c301a5f7b2c8ba
Saikat <saikats_5@yahoo.co.in>
2018-07-07 14:03:44 +0530

    bale installation

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 const css = require('./app.scss');
 
-console.log("Hello World and webpack dev server");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello World React</h1>,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ module.exports = {
     },
     module: {
         rules: [
-            {test: /\.scss$/, 
-            use: ['style-loader','css-loader','sass-loader']}
+            {test: /\.scss$/, use: ['style-loader','css-loader','sass-loader']},
+            {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
         ]
     },
     devServer: {
